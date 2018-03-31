1735edebf88bacaeced6519ffc1c3184bb07121d
apt-yoshi <cppatrick123@gmail.com>
2018-03-30 22:14:10 -0400

    h

--- app/src/main/java/com/example/trailmix/MainActivity.java
+++ app/src/main/java/com/example/trailmix/MainActivity.java
@@ -1,7 +1,11 @@
 package com.example.trailmix;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,6 +13,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        //what the fiddle stick
+
+    }
+    public void getDuration(View v){
+        EditText timeText = findViewById(R.id.editText);
+        String timeStr = timeText.getText().toString();
+
+        Log.d("MainActivity", timeStr);
+        Double timeD = Double.parseDouble(timeStr);
+
+        Intent intent = new Intent(this,TimeActivity.class);
+        intent.putExtra("time", timeD);
+        startActivity(intent);
     }
 }
