1402465afcaa3e615d7e32be431fffdaa0205014
acgoering <andrew.goering1@gmail.com>
2018-03-31 17:04:37 -0400

    Small UI Fix

--- app/src/main/java/com/example/trailmix/MainActivity.java
+++ app/src/main/java/com/example/trailmix/MainActivity.java
@@ -16,11 +16,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int targetTime = 60 * 60;
+       /* int targetTime = 60 * 60;
         Log.d("PGA", "hi there");
         Song[] songs = PlaylistGenerator.createRandomSongList(5, 120, 300);
         Log.d("PGA","Songs: " + Arrays.toString(songs));
-        Log.d("PGA", "Playlist: " + PlaylistGenerator.generatePlaylist(new ArrayList<Song>(Arrays.asList(songs)), targetTime));
+        Log.d("PGA", "Playlist: " + PlaylistGenerator.generatePlaylist(new ArrayList<Song>(Arrays.asList(songs)), targetTime));*/
 
     }
     public void getDuration(View v){
