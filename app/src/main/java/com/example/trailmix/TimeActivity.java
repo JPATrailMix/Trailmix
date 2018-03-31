6795a50167ef30bbc2a627af53751d6b09c30565
apt-yoshi <cppatrick123@gmail.com>
2018-03-30 23:38:28 -0400

    here

--- app/src/main/java/com/example/trailmix/TimeActivity.java
+++ app/src/main/java/com/example/trailmix/TimeActivity.java
@@ -1,12 +1,16 @@
 package com.example.trailmix;
 
 import android.content.Intent;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class TimeActivity extends AppCompatActivity {
 
@@ -16,7 +20,7 @@ public class TimeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_time);
         Bundle bundle = getIntent().getExtras();
 
-        Double time = bundle.getDouble("time");
+        /*Double time = bundle.getDouble("time");
         TextView timeView = findViewById(R.id.timeView);
         while(time > 0){
             timeView.setText(time.toString());
@@ -26,7 +30,25 @@ public class TimeActivity extends AppCompatActivity {
                 e.printStackTrace();
             }
             time--;
-        }
+        }*/
+        final TextView tv = (TextView) findViewById( R.id.textView2);
+        new CountDownTimer(20*60000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                tv.setText("seconds remaining: " +new SimpleDateFormat("mm:ss:SS").format(new Date( millisUntilFinished)));
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+
+/*
+            public void onFinish() {
+                tv.setText("done!");
+            }
+*/
+        }.start();
         Intent intent2 = new Intent(this,FinishedActivity.class);
         startActivity(intent2);
 
