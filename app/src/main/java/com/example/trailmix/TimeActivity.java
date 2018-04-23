167f9a624dcab7686d8b481b0624b0b2ccaed8b8
acgoering <andrew.goering1@gmail.com>
2018-04-23 14:40:55 -0400

    Makes playlists more random and makes them include longer songs more often.

--- app/src/main/java/com/example/trailmix/TimeActivity.java
+++ app/src/main/java/com/example/trailmix/TimeActivity.java
@@ -1,6 +1,7 @@
 package com.example.trailmix;
 
 import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,38 +10,61 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import java.io.IOException;
 import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 
 public class TimeActivity extends AppCompatActivity {
-
+    private SongsPlayer songsPlayer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_time);
         Bundle bundle = getIntent().getExtras();
-        Log.d("Countdown", "Countdown about to begin");
-        Double time = bundle.getDouble("time");
-        int millis = (int)(time*60*1000);
-        new CountDownTimer(millis, 1000) {
-            TextView tv = (TextView) findViewById(R.id.time);
-            public void onTick(long millisUntilFinished) {
-                long seconds = millisUntilFinished / 1000;
-                if(seconds%60 < 10){
-                    tv.setText(""+ seconds/60 +  ":0" + seconds%60);
-                }
-                else {
-                    tv.setText("" + seconds / 60 + ":" + seconds % 60);
-                }
-                Log.d("Countdown", "" + seconds);
-            }
 
-            public void onFinish() {
-                tv.setText("done!");
-                startFinishedActivity();
-            }
-        }.start();
+        double time = bundle.getDouble("MinutesTime");
+        SongRetriever sr = new SongRetriever(this);
+        long startTime = System.currentTimeMillis();
+
+        ArrayList<Song> songs = PlaylistGenerator.generateSongs(sr.getSongNames(),sr.getSongIds(),sr.getSongLengths());
+        try {
+            Playlist playlist = PlaylistGenerator.generatePlaylist(songs, (int) (time * 60));
+
+            int targetTime = (int) (time * 60);
+            Log.d("Music", "hi there, target time is " + targetTime + "s");
+            Log.d("Music", "Playlist: " + playlist);
+            Log.d("Music", "Entire PGA took " + (System.currentTimeMillis() - startTime) + "ms to run");
+
+            songsPlayer = new SongsPlayer(playlist.getSongs(), this);
+
+            Log.d("Countdown", "Countdown about to begin");
+            int millis = (int) (time * 60 * 1000);
+            new CountDownTimer(millis, 1000) {
+                TextView tv = (TextView) findViewById(R.id.time);
+
+                public void onTick(long millisUntilFinished) {
+                    long seconds = millisUntilFinished / 1000;
+                    if (seconds % 60 < 10) {
+                        tv.setText("" + seconds / 60 + ":0" + seconds % 60);
+                    } else {
+                        tv.setText("" + seconds / 60 + ":" + seconds % 60);
+                    }
+                    Log.d("Countdown", "" + seconds);
+                }
+
+                public void onFinish() {
+                    tv.setText("done!");
+                    startFinishedActivity();
+                }
+            }.start();
+        }
+        catch(IOException e){
+            Toast passiveAgressive = Toast.makeText(getApplicationContext(),"No songs found in native storage. Get a life.",Toast.LENGTH_LONG);
+            passiveAgressive.show();
+        }
     }
     private void startFinishedActivity(){
         Intent intent2 = new Intent(this,FinishedActivity.class);
@@ -59,6 +83,7 @@ public class TimeActivity extends AppCompatActivity {
         nonconfirm.setVisibility(View.VISIBLE);
     }
     public void onYes(View view){
+        songsPlayer.stop();
         Intent intent2 = new Intent(this,FinishedActivity.class);
         startActivity(intent2);
     }
